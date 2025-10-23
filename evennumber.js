73e560f2bb3a3ccfcab8ad67e2026f17e83cafda
Khursheed Khawar <r.khursheedkhawar@gmail.com>
2025-10-23 23:20:16 +0500

    Add new practice files

---
+++ evennumber.js
@@ -0,0 +1,8 @@
+let m_number = 7;
+let u_number = prompt("Enter Number between 1 to 10");
+
+while (m_number != u_number) {
+  u_number = prompt("Wrong: Enter Number between 1 to 10");
+}
+
+console.log("You Got It");
