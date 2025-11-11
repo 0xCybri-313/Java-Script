f2baf7a1a2269281f6b11e59022b84dfe73cf637
0xCybri-313 <r.khursheedkhawar@gmail.com>
2025-11-11 21:02:05 +0500

    Add count functionality to this programme

--- counter_app/main.js
+++ counter_app/main.js
@@ -1,3 +1,12 @@
+let count_digit = 0;
+function count_number() {
+  count_digit += 1;
+  if (count_digit <= 10) {
+    return count_digit;
+  } else {
+    return "No more tickets";
+  }
+}
 function count_down() {
-  document.getElementById("count_down").innerText = 99;
+  document.getElementById("count_down").innerText = count_number();
 }
