eeb7bccfc678e44f9e703bb18b1177eeadafaffe
0xCybri-313 <r.khursheedkhawar@gmail.com>
2025-11-13 23:57:56 +0500

    Update Element Selector to Query Selector

--- Blackjack/main.js
+++ Blackjack/main.js
@@ -1,6 +1,23 @@
-// 1. Create two variables, firstCard and secondCard.
-// Set their values to a random number between 2-11
-let firstCard = 5;
-let secondCard = 17;
-// 2. Create a variable, sum, and set it to the sum of the two cards
+let firstCard = 10;
+let secondCard = 4;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+
+let messageEl = document.querySelector("#message-el");
+let sumEl = document.querySelector("#sum-el");
+
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  sumEl.textContent = "Sum: " + sum;
+  messageEl.textContent = message;
+}
