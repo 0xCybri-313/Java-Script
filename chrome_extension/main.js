d57c0f8dd63e4e92412845174793e857eae9d877
0xCybri-313 <r.khursheedkhawar@gmail.com>
2025-12-13 01:08:16 +0500

    Working with Local Storage for save links in ch-ex

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -1,21 +1,38 @@
+// ============= Declaration ============= //
+
 const inputBtn = document.querySelector("#input-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
 let myLeads = [];
 
+// ============= Function ============= //
+
 function renderLeads() {
-  ulEl.innerHTML += `
+  let listItems = "";
+  for (let lead of myLeads) {
+    listItems += `
     <li>
-      <a href="#" target="_blank">
-        ${inputEl.value} 
+      <a href="${lead}" target="_blank">
+        ${lead} 
       </a> 
     </li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+// ============= Main ============= //
+
+let leadFormLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+if (leadFormLocalStorage) {
+  myLeads = leadFormLocalStorage;
+  renderLeads();
 }
 
 inputBtn.addEventListener("click", () => {
   console.log("Button Clicked");
   if (inputEl.value !== "") {
     myLeads.push(inputEl.value);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
     renderLeads();
     inputEl.value = "";
   }
