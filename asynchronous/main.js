9ca173c8ccab8b0bcad04b21d45452f8ce23b647
0xCybri-313 <r.khursheedkhawar@gmail.com>
2025-12-16 19:38:53 +0500

    just fetch data from random website

---
+++ asynchronous/main.js
@@ -0,0 +1,11 @@
+const getAllProducts = async () => {
+  try {
+    const response = await fetch("https://dummyjson.com/products/1");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getAllProducts();
