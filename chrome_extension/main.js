961c124ef9826ebd17805e008671e1c630959305
0xCybri-313 <r.khursheedkhawar@gmail.com>
2025-12-18 00:18:33 +0500

    Finalized chrome extension

--- chrome_extension/main.js
+++ chrome_extension/main.js
@@ -4,13 +4,15 @@ const inputBtn = document.querySelector("#input-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 const inputEl = document.querySelector("#input-el");
 const ulEl = document.querySelector("#ul-el");
+const tabBtn = document.querySelector("#tab-btn");
 let myLeads = [];
+const tabs = [{ url: "https://www.linkedin.com/in/per-harald-borgen/" }];
 
 // ============= Function ============= //
 
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let lead of myLeads) {
+  for (let lead of leads) {
     listItems += `
     <li>
       <a href="${lead}" target="_blank">
@@ -26,7 +28,7 @@ function renderLeads() {
 const leadFormLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 if (leadFormLocalStorage) {
   myLeads = leadFormLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
 inputBtn.addEventListener("click", () => {
@@ -34,13 +36,21 @@ inputBtn.addEventListener("click", () => {
   if (inputEl.value !== "") {
     myLeads.push(inputEl.value);
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderLeads();
+    render(myLeads);
     inputEl.value = "";
   }
+});
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
 deleteBtn.addEventListener("dblclick", () => {
   localStorage.clear();
   myLeads = [];
-  renderLeads();
+  render(myLeads);
 });
